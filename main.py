9e6d9112593b9221d8a44dd09845468bea5e00b9
Keenan Cunningham <kcunn021@fiu.edu>
2024-08-15 15:11:59 -0400

    Added a dictionary to the House class, and wrote some functions.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pandas as pd
 from src.classes.house import House
-from src.utils.read_write import read_from_file, write_to_file
+from src.utils.read_write import *
 
 INPUT_FILE: str = "housing_data.csv"
-OUTPUT_FILE: str = "housing_predictor_out.txt"
+OUTPUT_FILE: str = "housing_predictor_out.csv"
 
 house_list, house_dict = read_from_file(INPUT_FILE)
 
-write_to_file(house_list, house_dict, OUTPUT_FILE)
+write_to_file(OUTPUT_FILE)
 
+house_id = 1493
+
+delete_house_by_id(house_id)
+
+house_list, house_dict = read_from_file(OUTPUT_FILE)
+
+print("yay")
--- src/classes/house.py
+++ src/classes/house.py
@@ -1,3 +1,8 @@
+import re
+
+def print_month(month):
+    month = month.strip('/')
+    return "".join(["0", month]) if (int(month) < 9) else month
 
 class House:
     def __init__(self, id: int, neighborhood: str, house_style: str, overall_condition: int,
@@ -19,22 +24,23 @@ class House:
         self.garage_area = garage_area
         self.date_sold = f"{month}/{year}"
 
-    def __repr__(self) -> str:
-        # TODO: Make representation more verbosef
-        return f"House #{self.id}:\n"
+        self.properties: dict ={
+                "id" : id,
+                "neighborhood" : neighborhood,
+                "house_style" : house_style,
+                "overall_condition" : overall_condition,
+                "year_built" : year_built,
+                "roof_type" : roof_type,
+                "roof_material" : roof_material,
+                "foundation_material" : foundation_material,
+                "heating" : heating,
+                "central_air" : central_air,
+                "electrical" : electrical,
+                "fireplace_num" : fireplace_num,
+                "garage_area" : garage_area,
+                "date_sold" : f"{month}/{year}"
+        }
 
-    @classmethod
-    def update_house_by_id(house_id, updated_fields: dict) -> None:
-        pass
-
-    @classmethod
-    def delete_house_by_id(house_id: int) -> None:
-        """
-        Removes the house with the given house_id from the housing_dict dictionary.
-        """
-        housing_dict.pop(house_id)
-
-    @classmethod
-    def get_houses_by_filter(filters: list) -> list:
-        pass
+    def __repr__(self):
+        return f"{self.id},{self.neighborhood},{self.house_style},{self.overall_condition},{self.year_built},{self.roof_type},{self.roof_material},{self.foundation_material},{self.heating},{self.central_air},{self.electrical},{self.fireplace_num},{self.garage_area},{print_month(self.date_sold[:2])},{self.date_sold[-4:]}\n"
 
