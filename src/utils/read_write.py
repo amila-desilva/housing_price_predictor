a6a888d72bdb35f89a91665eb5dad5147b948998
Keenan Cunningham <103394924+HotelDangerous@users.noreply.github.com>
2024-08-16 23:25:46 -0400

    Add files via upload

--- src/utils/read_write.py
+++ src/utils/read_write.py
@@ -28,32 +28,44 @@ def write_to_file(file_name:str, housing_list: list=housing_list, housing_dict: 
     # open a file in write mode ('w') and write the House objects to that file
     output_file = open(file_name, 'w')
     
+    # make a string of all the attributes of a house
     labels: str = ("Id,Neighborhood,HouseStyle,OverallCond,YearBuilt,RoofStyle," + 
         "RoofMatl,Foundation,Heating,CentralAir,Electrical,Fireplaces,GarageArea," + 
         "MoSold,YrSold").replace('\n', '')
-    
-    print(labels)
 
-    output_file.write(labels + '\n')
+    output_file.write(labels + '\n')  # write the labels to a write file and end that line
+
+    # for each house in the housing_list, print its attributes to the write file
     for house in housing_list:
-        print(house.__repr__())
         output_file.write(house.__repr__())
 
 
 def update_house_by_id(house_id, updated_fields: dict, housing_dict: dict=housing_dict) -> None:
-    for key in updated_fields.keys():
-       pass 
+    house = housing_dict[house_id]                      # get the relevant house from the dictionary
+    for key in updated_fields.keys():                   # for each field we want to update
+        house.properties[key] = updated_fields[key]     # update that field using the house.properties dict
     
 
 def delete_house_by_id(house_id: int, housing_dict: dict=housing_dict, housing_list: list=housing_list) -> None:
     """
     Removes the house with the given house_id from the housing_dict dictionary and housing_list list.
     """
-    housing_list.remove(housing_dict[house_id])
-    housing_dict.pop(house_id)
+    housing_list.remove(housing_dict[house_id])  # remove house from list of houses
+    housing_dict.pop(house_id)                   # remove house from dictionary of houses
     
 
-def get_houses_by_filter(filters: list) -> list:
-    pass
+def get_houses_by_filter(filters: dict) -> list:
+    filtered_list = []          # create an empty list
+    for house in house_list:    
+        flag == True            # flag to notify us if an event occured
+        for key in filters.keys():
+            if filter[key] != house.properties[key]:  # if the house does not match the filter_list
+                flag = False                          # set flag to False
+                break                                 # break the loop, and check next house
+        if flag == True:               # if the flag is still True
+            filter_list.append(house)  # add it to filtered_list
+
+    return filter_list
+
 
 
